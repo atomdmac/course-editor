cd3e6d523ac8a0e96b17d07fb6172af7f044c0b6
suicidepills <atom.mac@gmail.com>
2014-02-03 12:44:23 -0500

    Implement basic course editor features.
    No polish but you can add tracks, chapters and pages.  No removals yet.
    
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/main.js
+++ js/main.js
@@ -3,24 +3,69 @@ $(document).ready(function () {
 });
 function App ($, ko) {
     
-    var Model = {
-        things: ko.observableArray([
-            {
-                name: /*ko.observable(*/"banana"/*)*/,
-                cost: ko.observable(29.39)
-            },
-            {
-                name: ko.observable("bat hair"),
-                cost: ko.observable(0.99)
-            },
-            {
-                name: ko.observable("beer coozy"),
-                cost: ko.observable(4.99)
-            }
-        ])
+    // Define Track, Chapter and Page models.
+    var Track = function (id, title, chapters, config) {
+        var self = this;
+        
+        this.id     = ko.observable(id     || "");
+        this.title  = ko.observable(title  || "DEFAULT TITLE");
+        this.config = ko.observable(config || {});
+        this.chapters = ko.observableArray(chapters || []);
+        
+        this.addChapter = function () {
+            self.chapters.push(new Chapter());
+        }
+        this.removeChapter = function () {
+            // TODO
+        }
     };
     
-    ko.applyBindings(Model);
+    var Chapter = function (id, title, pages, config) {
+        var self = this;
+        
+        this.id     = ko.observable(id || "");
+        this.title  = ko.observable(title || "New Chapter");
+        this.pages  = ko.observableArray(pages || []);
+        this.config = config || {};
+        
+        this.addPage = function () {
+            self.pages.push(new Page());
+        };
+        
+        this.removePage = function (page) {
+            // TODO
+        };
+    };
+    
+    var Page = function (id, title, path, config) {
+        var self = this;
+        
+        this.id     = ko.observable(id || "");
+        this.config = config || {};
+        this.title  = ko.observable(title || "New Page");
+        this.path   = ko.observable(path || "path/to/content/");
+    }
+    
+    // Define dummy model.
+    var CourseModel = function () {
+        this.selectedTrack   = ko.observable();
+        this.selectedChapter = ko.observable();
+        this.selectedPage    = ko.observable();
+        
+        this.tracks = ko.observableArray([
+            new Track("main", "Main Track", [
+                new Chapter("01", "Chapter 01", [
+                    new Page("0101")
+                ])
+            ])
+        ]);
+        
+        this.addTrack = function () {
+            this.tracks.push(new Track());
+        };
+    }
+    
+    ko.applyBindings(CourseModel);
 }
 // 6089080149487118
 // 61323843
